1105ee20370b41cd956e1e59f574a626ac44e206
John Marc Balbada <jmarcbalbada@gmail.com>
2024-09-16 14:57:09 +0800

    Add project files.

---
+++ CreateDbFromScratch/Model/SchoolContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CreateDbFromScratch.Model
+{
+    public class SchoolContext : DbContext
+    {
+        // create table
+        public DbSet<Student> Students { get; set; }
+
+        public SchoolContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+    }
+}
